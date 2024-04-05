2d34e0217850492ae650f6ac61468f0e430abe8d
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-04-05 19:43:56 +0530

    njnjnjni

--- api/index.js
+++ api/index.js
@@ -127,8 +127,8 @@ app.put("/api/terms/:id", async (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
 });
 
