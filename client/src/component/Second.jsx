d71bd71c6945a01111169b5b7cfe8f164aeef57e
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-04-06 14:32:17 +0530

    background

--- client/src/component/Second.jsx
+++ client/src/component/Second.jsx
@@ -1,41 +1,80 @@
-import { Button, Card } from "flowbite-react";
+/* eslint-disable no-unused-vars */
+import { Button } from "flowbite-react";
 import serviqrr from "../asset/serviqr.png";
 import serviqrmobile from "../asset/serviqrmobile.png";
 import first from "../asset/first.png";
+import what from "../asset/whatsssss.png";
 //add icon for arrow rignt tilted upward react icons
 
 import { FaArrowRight } from "react-icons/fa";
 function Second() {
   return (
-    <div className="bg-white text-black">
-      <Card className="shadow-none border-none">
-        <div className="flex flex-col items-center justify-center">
-          <div className="font-abhaya-libre text-3xl md:text-5xl lg:text-6xl font-semibold leading-tight text-center pt-20 md:w-3/5">
-            <p>One smart Conversational AI tool for your business</p>
-          </div>
-          <div className="font-montserrat text-base text-m md:text-lg lg:text-xl font-normal leading-normal text-center pt-10">
-            <p>
-              Get ready for effortless marketing, sales, and customer
-              conversations
-            </p>
-          </div>
+    <div className="bg-white text-black bg-no-repeat ">
+      {/* <div
+        style={{
+          backgroundImage: `url(${what})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height:"555px",
+          width:"555px"
+        }}
+      ></div> */}
 
-          {/* Center the button container */}
-          <div className="pt-12">
-            <Button color="success" className="bg-[#12bf7c]" size="xl">
-              Book a Demo
-              <FaArrowRight className="transform rotate-270 m-1 ml-2" />
-            </Button>
-          </div>
+      <div
+        className="absolute bg-cover"
+        style={{
+          backgroundImage: `url('${what}')`,
+          width: "658px", // Adjust width as needed
+          height: "658px", // Adjust height as needed
+          top: "100px", // Adjust position as needed
+          left: "-260px",
+          // Adjust position as needed
+          opacity: "0.06",
+        }}
+      ></div>
+      <div
+        className="absolute bg-cover"
+        style={{
+          backgroundImage: `url('${what}')`,
+          width: "489px", // Adjust width as needed
+          height: "489px", // Adjust height as needed
+          top: "247px", // Adjust position as needed
+          left: "1150px",
+          // Adjust position as needed
+          opacity: "0.06",
+        }}
+      ></div>
+
+      <div
+        className="flex flex-col items-center bg-no-repeat justify-center
+      "
+      >
+        <div className="font-abhaya-libre text-3xl md:text-5xl lg:text-6xl font-semibold leading-tight text-center pt-20 md:w-3/5">
+          <p>One smart Conversational AI tool for your business</p>
         </div>
-      </Card>
+        <div className="font-montserrat text-base text-m md:text-lg lg:text-xl font-normal leading-normal text-center pt-10">
+          <p>
+            Get ready for effortless marketing, sales, and customer
+            conversations
+          </p>
+        </div>
+
+        {/* Center the button container */}
+        <div className="pt-12">
+          <Button color="success" className="bg-[#12bf7c]" size="xl">
+            Book a Demo
+            <FaArrowRight className="transform rotate-270 m-1 ml-2" />
+          </Button>
+        </div>
+      </div>
 
       {/* first part of the web */}
       <div className="flex justify-center mt-10 md:mt-20">
         <img
-          src={first} // Replace with the path to your image file
+          src="https://framerusercontent.com/images/D6CTA1pnxhaC0a4Nhx7rUtxz4.png" // Replace with the path to your image file
           alt="Your Image"
-          className="w-full md:w-3/5 "
+          className="md:w-4/5 "
         />
       </div>
 
