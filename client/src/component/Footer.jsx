c72bb2274d22740247040b173fd25c6dfcead59e
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-04-09 13:07:44 +0530

    dcw

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -1,12 +1,26 @@
 import { Footer } from "flowbite-react";
-import logo from "../asset/servii_logo.svg";
+import logo from "../asset/servii_logo_footer.svg";
 function Footerbars() {
   return (
     <div>
-      <Footer className="bg-black text-white " container>
-        <div className="">
-          <Footer.Brand href="#" src={logo} alt="Flowbite Logo"
-            name="Flowbite" />
+      <Footer className="bg-black text-white w-full " container>
+        <div className="  w-full justify-between ">
+          <Footer.Brand
+            href="#"
+            src={logo}
+            alt="Flowbite Logo"
+          />
+          <div className="">
+            <Footer.LinkGroup>
+              <Footer.Link href="#">About us</Footer.Link>
+              <Footer.Link href="#">Our Services</Footer.Link>
+
+              <Footer.Link href="#">FAQs</Footer.Link>
+              <Footer.Link href="#">Contact us</Footer.Link>
+              <Footer.Link href="#">Privacy & Policy</Footer.Link>
+              <Footer.Link href="#">Terms & Condition</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
         </div>
       </Footer>
     </div>
